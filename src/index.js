d032e9ba7fec649e9a79caa8f26c19bd407617c3
Faustino Ruiz <ftr1989@gmail.com>
2024-04-21 09:20:52 -0700

    reactDom

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { store } from './store'
 import { Provider } from 'react-redux'
 
+//after this line of code, you can use React to define and render React components, and use ReactDOM to render them to the DOM.
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
